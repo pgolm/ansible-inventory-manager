7121c734c7b35a7ea68b7e80f8e0c17cb9f6325c
Peter Golm <golm.peter@gmail.com>
2013-09-27 17:43:34 +0200

    add feature: pace site load indicator

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require jquery.turbolinks
 //= require turbolinks
+//= require pace.min
 //= require twitter/bootstrap/alert
 //= require twitter/bootstrap/button
 //= require twitter/bootstrap/collapse
