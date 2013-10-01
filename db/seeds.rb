635ecc92e2aa9a806b89ab80320c98730e346ea2
Peter Golm <golm.peter@gmail.com>
2013-10-01 10:01:37 +0200

    add: default admin user to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Identity.create(email: 'admin@example.com', 
+                password: 'admin', 
+                password_confirmation: 'admin')
+
+User.create(provider:"identity", 
+            uid: 1, 
+            email: 'admin@example.com', 
+            is_admin: true)
