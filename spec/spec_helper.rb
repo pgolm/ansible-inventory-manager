3c03b7529f319524d29bcc91856b28055cefb153
Peter Golm <golm.peter@gmail.com>
2013-12-17 21:48:25 +0100

    fix coveralls testsuit setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,12 @@
+require 'coveralls'
+Coveralls.wear!('rails')
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
-require 'coveralls'
-Coveralls.wear!('rails')
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
