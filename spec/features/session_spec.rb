338853326f3b9c36e2d3edc3be38eaf434477800
Peter Golm <golm.peter@gmail.com>
2013-12-18 14:57:27 +0100

    add access denied test to login feature

--- spec/features/session_spec.rb
+++ spec/features/session_spec.rb
@@ -3,13 +3,24 @@ require "spec_helper"
 feature "Session" do
   let(:user) { create(:identity) }
 
-  scenario "Login" do
-    visit "/signin"
+  feature "Login" do
+    background do
+      visit "/signin"
+      fill_in "Email", with: user.email
+    end
 
-    fill_in "Email", with: user.email
-    fill_in "Password", with: user.password
-    click_button "Login"
+    scenario "Success" do
+      fill_in "Password", with: user.password
+      click_button "Login"
 
-    expect(page).to have_text("Signed in!")
+      expect(page).to have_text("Signed in!")
+    end
+
+    scenario "Denied" do
+      fill_in "Password", with: "#{user.password}_failed"
+      click_button "Login"
+
+      expect(page).to have_text("not authorized")
+    end
   end
 end
