df719905706fc32e10e0d0e7324dd2696c723d6a
Peter Golm <golm.peter@gmail.com>
2013-12-18 14:53:06 +0100

    add capybara

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,8 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
-
+require 'capybara/rails'
+require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
