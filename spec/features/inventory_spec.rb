0c13512b5d07b96d1f08f9691ff4e393e8346f73
Peter Golm <golm.peter@gmail.com>
2013-12-18 18:18:58 +0100

    improve coverage :D

--- spec/features/inventory_spec.rb
+++ spec/features/inventory_spec.rb
@@ -12,4 +12,41 @@ feature "Inventory" do
     visit inventories_path
     expect(page).to have_text(inventory.name)
   end
+
+  context "Add Inventory" do
+    before do
+      visit inventories_path
+      click_link "new_inventory"
+    end
+
+    scenario "Success" do
+      fill_in "Name", with: "#{inventory.name}_new"
+      click_on "Create"
+      
+      expect(page).to have_text("Inventory was created.")
+    end
+
+    scenario "Fail" do
+      fill_in "Name", with: inventory.name
+      click_on "Create"
+
+      expect(page).to have_text("has already")
+    end
+  end
+
+  scenario "Edit Inventory" do
+    visit inventories_path
+    click_link "edit_inventory"
+    fill_in "Name", with: "#{inventory.name}_edit"
+    click_on "Update"
+    
+    expect(page).to have_text("Inventory was updated.")
+  end
+
+  scenario "Delete Inventory" do
+    visit inventories_path
+    click_link "delete_#{inventory.name}"
+    
+    expect(page).to have_text("Inventory was deleted.")
+  end
 end
